903b9bd22255b27466cc0d4200f0ae825605a5bd
Ali Reza Baqeri <alirezabaqeri972@gmail.com>
2026-09-10 18:22:22 +0200

    feat: connect NestJS to MongoDB with Mongoose

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { StationsModule } from './stations/stations.module';
 
 @Module({
-  imports: [StationsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/ev-stations'),
+    StationsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
